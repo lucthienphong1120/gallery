e2236ebbe6195b642487aafc239d1fc4969f250d
Lục Thiên Phong <chung.bkc13061@student.bkacad.edu.vn>
2022-12-25 14:56:45 +0700

    Automatic update by sync.bat

--- sync.bat
+++ sync.bat
@@ -2,10 +2,11 @@
 color 2
 git pull
 python setup.py
+echo ---------------------------------------------------
 git add -A
 git commit -m "Automatic update by sync.bat"
 git push
 git pull
 echo Sync successful! Thank you for using
-echo .
+echo ---------------------------------------------------
 pause

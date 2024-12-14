18c8fba0b22a7a830ab48dc39fcc2db8fd378e4b
Thien Phong <aichoibangbangko@gmail.com>
2024-12-14 22:52:55 +0700

    Automatic updated by sync.bat

--- sync.bat
+++ sync.bat
@@ -1,10 +1,12 @@
 @echo off
 color 2
+echo Check for lastest update from Repo
 git pull
+echo Start processing images to data
 python setup.py
 echo ---------------------------------------------------
 git add -A
-git commit -m "Automatic update by sync.bat"
+git commit -m "Automatic updated by sync.bat"
 git push
 git pull
 echo Sync successfully! Thanks for using this script!
